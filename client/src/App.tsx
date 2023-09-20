d601be79f01e0165552a94aa28dfcb54633f10ad
Christofer Assis <chris.f.assis18@gmail.com>
2023-09-20 20:10:11 -0300

    feat: add PostList component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import { PostCreate } from './components/PostCreate';
+import { PostList } from './components/PostList';
 
 export function App() {
 	return (
 		<div className="container">
 			<h1>Create Post</h1>
 			<PostCreate />
+
+			<hr />
+
+			<h1>Posts</h1>
+			<PostList />
 		</div>
 	);
 }
