d59479b9a131c7a926ba160e91bf01dea2c4d9f8
Christofer Assis <chris.f.assis18@gmail.com>
2023-09-21 21:40:11 -0300

    refactor: change content value based on it status

--- client/src/components/CommentList.tsx
+++ client/src/components/CommentList.tsx
@@ -1,11 +1,26 @@
 export type Comments = {
 	id: string;
 	content: string;
+	status: 'approved' | 'pending' | 'rejected';
 };
 
 export function CommentList({ comments }: { comments: Comments[] }) {
 	const renderedComments = comments.map((comment) => {
-		return <li key={comment.id}>{comment.content}</li>;
+		let content: string = '';
+
+		if (comment.status === 'approved') {
+			content = comment.content;
+		}
+
+		if (comment.status === 'pending') {
+			content = 'This comment is awaiting moderation';
+		}
+
+		if (comment.status === 'rejected') {
+			content = 'This comment has been rejected';
+		}
+
+		return <li key={comment.id}>{content}</li>;
 	});
 
 	return <ul>{renderedComments}</ul>;
