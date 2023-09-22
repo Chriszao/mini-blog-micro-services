931796ea1edc96261bf6576fd29d3b386952e50d
Christofer Assis <chris.f.assis18@gmail.com>
2023-09-21 22:04:45 -0300

    feat: implement event syncing when query service is down

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 async function callService(port, event) {
 	const baseUrl = `http://localhost:${port}/events`; // For this purpose, we are running the apps in the same machine
 
@@ -15,12 +17,18 @@ async function callService(port, event) {
 app.post('/events', async (req, res) => {
 	const event = req.body;
 
+	events.push(event);
+
 	callService(4000, event); // posts
 	callService(4001, event); // comments
 	callService(4002, event); // query
 	callService(4003, event); // moderation
 
 	res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+	res.send(events);
 });
 
 app.listen(4005, () => console.log('Running on port 4005'));
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
@@ -9,13 +10,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (_, res) => {
-	res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-	const { type, data } = req.body;
-
+function handleEvent(type, data) {
 	if (type === 'PostCreated') {
 		const { id, title } = data;
 
@@ -48,10 +43,32 @@ app.post('/events', (req, res) => {
 		comment.status = status;
 		comment.content = content;
 	}
+}
 
-	console.log(posts);
+app.get('/posts', (_, res) => {
+	res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+	const { type, data } = req.body;
+
+	handleEvent(type, data);
 
 	res.send({});
 });
 
-app.listen(4002, () => console.log('Listening on port 4002'));
+app.listen(4002, async () => {
+	console.log('Listening on port 4002');
+
+	try {
+		const response = await axios.get('http://localhost:4005/events');
+
+		for (const event of response.data) {
+			console.log('Processing event:', event.type);
+
+			handleEvent(event.type, event.data);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+});
